cb5b1f37c42751bc6e78316ef682f58042761b55
Omar Rodríguez <oarozu@gmail.com>
2021-06-27 18:01:48 -0300

    fix form buscar

--- src/Admin/UserBundle/Form/BuscarType.php
+++ src/Admin/UserBundle/Form/BuscarType.php
@@ -3,7 +3,6 @@
 namespace Admin\UserBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -14,7 +13,7 @@ class BuscarType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
                 $builder
-                ->add(TextType::class)
+                ->add('texto')
                 ->add('parametro', ChoiceType::class, array(
                 'placeholder' => 'Buscar por:',
                 'choices'   => array('Cédula' => 'ced', 'Nombres' => 'nom','Apellidos' => 'apell'),
