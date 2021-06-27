52b4f396c918f646b5ae866e49a5def40cce20d8
Omar Rodríguez <oarozu@gmail.com>
2021-06-27 17:15:29 -0300

    cambio orden opciones evaluacion

--- src/Admin/MedBundle/Form/coevalDirectorType.php
+++ src/Admin/MedBundle/Form/coevalDirectorType.php
@@ -18,39 +18,39 @@ class coevalDirectorType extends AbstractType
     {
         $builder
             ->add('f1', ChoiceType::class, array('placeholder' => '-', 'label' => ' ',
-            'choices'   => array( '5' => 'Siempre', '4' => 'Casi Siempre', '3' => 'Aveces', '2' => 'Casi Nunca', '1' => 'Nunca', '0' => 'No Aplica',), 'required'  => true,))
+            'choices'   => array( 'Siempre' => '5', 'Casi Siempre' => '4', 'Aveces' => '3', 'Casi Nunca' => '2', 'Nunca' => '1', 'No Aplica' => '0',), 'required'  => true,))
             ->add('f2', ChoiceType::class, array('placeholder' => '-', 'label' => ' ',
-            'choices'   => array( '5' => 'Siempre', '4' => 'Casi Siempre', '3' => 'Aveces', '2' => 'Casi Nunca', '1' => 'Nunca', '0' => 'No Aplica',), 'required'  => true,))
+            'choices'   => array( 'Siempre' => '5', 'Casi Siempre' => '4', 'Aveces' => '3', 'Casi Nunca' => '2', 'Nunca' => '1', 'No Aplica' => '0',), 'required'  => true,))
             ->add('f3', ChoiceType::class, array('placeholder' => '-', 'label' => ' ',
-            'choices'   => array( '5' => 'Siempre', '4' => 'Casi Siempre', '3' => 'Aveces', '2' => 'Casi Nunca', '1' => 'Nunca', '0' => 'No Aplica'), 'required'  => true,))
+            'choices'   => array( 'Siempre' => '5', 'Casi Siempre' => '4', 'Aveces' => '3', 'Casi Nunca' => '2', 'Nunca' => '1', 'No Aplica' => '0'), 'required'  => true,))
             ->add('f4', ChoiceType::class, array('placeholder' => '-', 'label' => ' ',
-            'choices'   => array( '5' => 'Siempre', '4' => 'Casi Siempre', '3' => 'Aveces', '2' => 'Casi Nunca', '1' => 'Nunca', '0' => 'No Aplica'), 'required'  => true,))
+            'choices'   => array( 'Siempre' => '5', 'Casi Siempre' => '4', 'Aveces' => '3', 'Casi Nunca' => '2', 'Nunca' => '1', 'No Aplica' => '0'), 'required'  => true,))
             ->add('f5', ChoiceType::class, array('placeholder' => '-', 'label' => ' ',
-            'choices'   => array( '5' => 'Siempre', '4' => 'Casi Siempre', '3' => 'Aveces', '2' => 'Casi Nunca', '1' => 'Nunca', '0' => 'No Aplica'), 'required'  => true,))
+            'choices'   => array( 'Siempre' => '5', 'Casi Siempre' => '4', 'Aveces' => '3', 'Casi Nunca' => '2', 'Nunca' => '1', 'No Aplica' => '0'), 'required'  => true,))
             ->add('f6', ChoiceType::class, array('placeholder' => '-', 'label' => ' ',
-            'choices'   => array( '5' => 'Siempre', '4' => 'Casi Siempre', '3' => 'Aveces', '2' => 'Casi Nunca', '1' => 'Nunca', '0' => 'No Aplica'), 'required'  => true,))
+            'choices'   => array( 'Siempre' => '5', 'Casi Siempre' => '4', 'Aveces' => '3', 'Casi Nunca' => '2', 'Nunca' => '1', 'No Aplica' => '0'), 'required'  => true,))
             ->add('f7', ChoiceType::class, array('placeholder' => '-', 'label' => ' ',
-            'choices'   => array( '5' => 'Siempre', '4' => 'Casi Siempre', '3' => 'Aveces', '2' => 'Casi Nunca', '1' => 'Nunca', '0' => 'No Aplica'), 'required'  => true,))
+            'choices'   => array( 'Siempre' => '5', 'Casi Siempre' => '4', 'Aveces' => '3', 'Casi Nunca' => '2', 'Nunca' => '1', 'No Aplica' => '0'), 'required'  => true,))
             ->add('f8', ChoiceType::class, array('placeholder' => '-', 'label' => ' ',
-            'choices'   => array( '5' => 'Siempre', '4' => 'Casi Siempre', '3' => 'Aveces', '2' => 'Casi Nunca', '1' => 'Nunca', '0' => 'No Aplica'), 'required'  => true,))
+            'choices'   => array( 'Siempre' => '5', 'Casi Siempre' => '4', 'Aveces' => '3', 'Casi Nunca' => '2', 'Nunca' => '1', 'No Aplica' => '0'), 'required'  => true,))
             ->add('f9', ChoiceType::class, array('placeholder' => '-', 'label' => ' ',
-            'choices'   => array( '5' => 'Siempre', '4' => 'Casi Siempre', '3' => 'Aveces', '2' => 'Casi Nunca', '1' => 'Nunca', '0' => 'No Aplica'), 'required'  => true,))
+            'choices'   => array( 'Siempre' => '5', 'Casi Siempre' => '4', 'Aveces' => '3', 'Casi Nunca' => '2', 'Nunca' => '1', 'No Aplica' => '0'), 'required'  => true,))
             ->add('f10', ChoiceType::class, array('placeholder' => '-', 'label' => ' ',
-            'choices'   => array( '5' => 'Siempre', '4' => 'Casi Siempre', '3' => 'Aveces', '2' => 'Casi Nunca', '1' => 'Nunca', '0' => 'No Aplica'), 'required'  => true,))
+            'choices'   => array( 'Siempre' => '5', 'Casi Siempre' => '4', 'Aveces' => '3', 'Casi Nunca' => '2', 'Nunca' => '1', 'No Aplica' => '0'), 'required'  => true,))
             ->add('f11', ChoiceType::class, array('placeholder' => '-', 'label' => ' ',
-            'choices'   => array( '5' => 'Siempre', '4' => 'Casi Siempre', '3' => 'Aveces', '2' => 'Casi Nunca', '1' => 'Nunca', '0' => 'No Aplica'), 'required'  => true,))
+            'choices'   => array( 'Siempre' => '5', 'Casi Siempre' => '4', 'Aveces' => '3', 'Casi Nunca' => '2', 'Nunca' => '1', 'No Aplica' => '0'), 'required'  => true,))
             ->add('f12', ChoiceType::class, array('placeholder' => '-', 'label' => ' ',
-            'choices'   => array( '5' => 'Siempre', '4' => 'Casi Siempre', '3' => 'Aveces', '2' => 'Casi Nunca', '1' => 'Nunca', '0' => 'No Aplica'), 'required'  => true,))
+            'choices'   => array( 'Siempre' => '5', 'Casi Siempre' => '4', 'Aveces' => '3', 'Casi Nunca' => '2', 'Nunca' => '1', 'No Aplica' => '0'), 'required'  => true,))
             ->add('f13', ChoiceType::class, array('placeholder' => '-', 'label' => ' ',
-            'choices'   => array( '5' => 'Siempre', '4' => 'Casi Siempre', '3' => 'Aveces', '2' => 'Casi Nunca', '1' => 'Nunca', '0' => 'No Aplica'), 'required'  => true,))
+            'choices'   => array( 'Siempre' => '5', 'Casi Siempre' => '4', 'Aveces' => '3', 'Casi Nunca' => '2', 'Nunca' => '1', 'No Aplica' => '0'), 'required'  => true,))
             ->add('f14', ChoiceType::class, array('placeholder' => '-', 'label' => ' ',
-            'choices'   => array( '5' => 'Siempre', '4' => 'Casi Siempre', '3' => 'Aveces', '2' => 'Casi Nunca', '1' => 'Nunca', '0' => 'No Aplica'), 'required'  => true,))
+            'choices'   => array( 'Siempre' => '5', 'Casi Siempre' => '4', 'Aveces' => '3', 'Casi Nunca' => '2', 'Nunca' => '1', 'No Aplica' => '0'), 'required'  => true,))
             ->add('f15', ChoiceType::class, array('placeholder' => '-', 'label' => ' ',
-            'choices'   => array( '5' => 'Siempre', '4' => 'Casi Siempre', '3' => 'Aveces', '2' => 'Casi Nunca', '1' => 'Nunca', '0' => 'No Aplica'), 'required'  => true,))
+            'choices'   => array( 'Siempre' => '5', 'Casi Siempre' => '4', 'Aveces' => '3', 'Casi Nunca' => '2', 'Nunca' => '1', 'No Aplica' => '0'), 'required'  => true,))
             ->add('f16', ChoiceType::class, array('placeholder' => '-', 'label' => ' ',
-            'choices'   => array( '5' => 'Siempre', '4' => 'Casi Siempre', '3' => 'Aveces', '2' => 'Casi Nunca', '1' => 'Nunca', '0' => 'No Aplica'), 'required'  => true,))
+            'choices'   => array( 'Siempre' => '5', 'Casi Siempre' => '4', 'Aveces' => '3', 'Casi Nunca' => '2', 'Nunca' => '1', 'No Aplica' => '0'), 'required'  => true,))
             ->add('f17', ChoiceType::class, array('placeholder' => '-', 'label' => ' ',
-            'choices'   => array( '5' => 'Siempre', '4' => 'Casi Siempre', '3' => 'Aveces', '2' => 'Casi Nunca', '1' => 'Nunca', '0' => 'No Aplica'), 'required'  => true,))
+            'choices'   => array( 'Siempre' => '5', 'Casi Siempre' => '4', 'Aveces' => '3', 'Casi Nunca' => '2', 'Nunca' => '1', 'No Aplica' => '0'), 'required'  => true,))
             ->add('observaciones', TextareaType::class, array('required'  => true, 'attr' => array('cols' => '120', 'maxlength' => '512')))
         ;
     }
