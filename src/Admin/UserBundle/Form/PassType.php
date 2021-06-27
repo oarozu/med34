0f7afe8a7b00305a3d8ea274be5904479d0f02d6
Omar Rodríguez <oarozu@gmail.com>
2021-06-27 13:28:08 -0300

    use choice type

--- src/Admin/UserBundle/Form/PassType.php
+++ src/Admin/UserBundle/Form/PassType.php
@@ -4,6 +4,7 @@ namespace Admin\UserBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class PassType extends AbstractType
