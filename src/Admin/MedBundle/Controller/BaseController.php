aa1c56b91a96be64baa445e981ed59e09068ca77
Omar Rodriguez <oarozu@gmail.com>
2023-12-26 18:51:14 -0300

    fix deprecated base controller

--- src/Admin/MedBundle/Controller/BaseController.php
+++ src/Admin/MedBundle/Controller/BaseController.php
@@ -5,8 +5,7 @@ namespace Admin\MedBundle\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Admin\MedBundle\Entity\Heteroeval;
 use Admin\MedBundle\Entity\HeteroevalRepository;
@@ -15,31 +14,27 @@ use Admin\MedBundle\Entity\HeteroevalRepository;
  * Base controller
  * @Route("/unad/doc")
  */
-class BaseController extends Controller {
-
+class BaseController extends Controller
+{
     /**
      * Mostrar heteroevaluaciones
-     * @Route("/{id}/heteroeval", name="heteroeval_info")
-     * @Method("GET")
-     * @Template()
+     * @Route("/{id}/heteroeval", name="heteroeval_info",  methods={"GET"})
+     * @Template("AdminMedBundle:Base:heteroeval.html.twig")
      */
-    public function heteroevalAction($id) {
+    public function heteroevalAction($id)
+    {
         $em = $this->getDoctrine()->getManager();
         $entity = $em->getRepository('AdminUnadBundle:Docente')->find($id);
         $evaluaciones = $em->getRepository('AdminMedBundle:Heterocursos')->findBy(array('cedula' => $entity->getUser()->getId(), 'semestre' => $entity->getPeriodo()));
-        
-        
-        if ($entity->getVinculacion()=='DOFE'){
-        $evaluaciones1 = $em->getRepository('AdminMedBundle:Heterocursos')->findBy(array('cedula' => $entity->getUser()->getId(), 'semestre' => $entity->getPeriodo()-1)); 
-        $evaluaciones = array_merge($evaluaciones1,$evaluaciones);
+
+        if ($entity->getVinculacion() == 'DOFE') {
+            $evaluaciones1 = $em->getRepository('AdminMedBundle:Heterocursos')->findBy(array('cedula' => $entity->getUser()->getId(), 'semestre' => $entity->getPeriodo() - 1));
+            $evaluaciones = array_merge($evaluaciones1, $evaluaciones);
         }
-       
-        
-        
+
         return array(
             'entity' => $entity,
             'evaluaciones' => $evaluaciones
         );
     }
-
 }
