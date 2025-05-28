25c0f53ff21c4d6aedc51bd777a4311cf131a749
Omar Rodriguez <oarozu@gmail.com>
2025-05-28 12:23:58 -0300

    fix periodoa on lider+

--- src/Controller/LiderController.php
+++ src/Controller/LiderController.php
@@ -55,7 +55,7 @@ class LiderController extends AbstractController
         $programas = $em->getRepository('App:Programa')->findBy(array('lider' => $user));
         $cursos = $em->getRepository('App:Curso')->findBy(array('programa' => $programas));
         $periodoe = $em->getRepository('App:Periodoe')->findBy(array('id' => $session->get('periodoe')));
-        #$periodoa = $em->getRepository('App:Periodoa')->findOneBy(array('periodoe' => $periodoe));
+        $periodoa = $em->getRepository('App:Periodoa')->findOneBy(array('periodoe' => $periodoe));
         $ofertas = $em->getRepository('App:Oferta')->findBy(array('curso' => $cursos, 'periodo' => $periodoa),array('director' => 'ASC'));
 
         return $this->render('Curso/lider.html.twig', array(
