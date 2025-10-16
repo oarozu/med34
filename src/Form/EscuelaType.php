6af365d22741d5bef604b4e042488c7cc36ae56b
Omar Rodriguez <oarozu@gmail.com>
2025-10-16 11:07:06 -0300

    fix type escuela

--- src/Form/EscuelaType.php
+++ src/Form/EscuelaType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Escuela;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
@@ -19,11 +20,11 @@ class EscuelaType extends AbstractType
             ->add('id')
             ->add('nombre')
             ->add('sigla')
-            ->add("decano", "text", array(
+            ->add("decano", TextType::class, array(
              "mapped" => false,
             'required'  => true,
                 ))
-            ->add("secretaria", "text", array(
+            ->add("secretaria", TextType::class, array(
              "mapped" => false,
             'required'  => true,
                 ))
